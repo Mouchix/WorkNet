3ea4a134a144754a293f8be48279556f6003c0d9
Marco Mondardini <marco.mondardini2@studio.unibo.it>
2026-05-06 11:50:24 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/worknet/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.worknet
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.worknet.ui.theme.WorkNetTheme
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            WorkNetTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val db = Firebase.firestore
+    db.collection("test").add(mapOf("hello" to "world"))
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    WorkNetTheme {
+        Greeting("Android")
+    }
+}
